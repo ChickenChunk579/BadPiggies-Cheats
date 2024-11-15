3530326867abb3fcb5fb26c108b9614ad1073a15
Rhys Tumelty <rhys@tumelty.co.uk>
2024-11-15 20:58:53 +0000

    Added files

---
+++ Assets/Scripts/Assembly-CSharp/FPSCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+	private void Awake()
+	{
+		
+	}
+
+	private void Update()
+	{
+		
+	}
+
+	private float updateInterval = 0.5f;
+
+	private float frames;
+
+	private float timeleft;
+
+	private Stopwatch m_stopwatch = new Stopwatch();
+}
---
+++ Assets/Scripts/Assembly-CSharp/HUD.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : WPFMonoBehaviour
+{
+	private void Start()
+	{
+		if (WPFMonoBehaviour.gameData.m_blueprintPrefab)
+		{
+			Transform transform = UnityEngine.Object.Instantiate<Transform>(WPFMonoBehaviour.gameData.m_blueprintPrefab, WPFMonoBehaviour.levelManager.StartingPosition, Quaternion.identity);
+			transform.name = "BlueprintUI";
+			transform.parent = WPFMonoBehaviour.levelManager.transform;
+			if (WPFMonoBehaviour.levelManager.m_blueprintTexture)
+			{
+				transform.GetComponent<RawImage>().texture = WPFMonoBehaviour.levelManager.m_blueprintTexture;
+			}
+		}
+	}
+}
